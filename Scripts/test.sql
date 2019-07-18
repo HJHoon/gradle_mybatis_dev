ae88580dfe6802b7fed5a1a2f55dea96fbb31289
현종훈 <hjh6219@gmail.com>
2019-07-18 17:01:16 +0900

    tutor,course 완료

--- Scripts/test.sql
+++ Scripts/test.sql
@@ -15,3 +15,10 @@ from students where stud_id=1;
 select * from addresses where addr_id = 3;
 select STUD_ID, name, email,phone, a.addr_id, street,city,state,zip,country
 from students s left join addresses a on s.addr_id = a.addr_id;
+
+select * from tutors;
+select * from courses;
+
+select t.tutor_id, t.name as tutor_name, email, c.course_id, c.name, description, start_date, end_date
+from tutors t left join courses c on t.tutor_id = c.tutor_id
+where t.tutor_id = 1;
